31d76f71f025095fb9a83a65ed315ce2f0ee2c63
Zain Usman <zainusman1856@gmail.com.com>
2025-04-27 21:09:05 +0500

    your commit message

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,7 +19,7 @@ function Header() {
                         <li><a href="#header">Home</a></li>
                         <li><a href="#about">About</a></li>
                         <li><a href="#services">Services</a></li>
-                        <li><a href="#portfolio">Portfolio</a></li>
+                        <li><a href="#portfolio">My Work</a></li>
                         <li><a href="#contact">Contact</a></li>
                     </ul>
                 </nav>
